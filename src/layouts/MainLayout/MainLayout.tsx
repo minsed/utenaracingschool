55caad4f9f20f15abd20411c066e2017fc55a4ac
Mindaugas Seduikis <mindaugas.seduik@gmail.com>
2021-06-09 15:27:49 +0300

    removed location because of build errors

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,20 +1,15 @@
-﻿import React, {createRef, FC, ReactNode, useEffect} from 'react';
+﻿import React, {FC, ReactNode} from 'react';
 import './MainLayout.scss'
 import HeaderContainer from '../../containers/HeaderContainer/HeaderContainer';
 
 const MainLayout: FC<MainLayoutProps> = ({children}: MainLayoutProps) => {
-    const contentRef = createRef<HTMLDivElement>();
 
-    useEffect(() => {
-        contentRef.current?.scrollTo(0, 0);
-    }, [location, contentRef]);
-    
     return <div className={"main-layout"}>
         <div className={"main-layout__header"}>
             <HeaderContainer />
         </div>
         <div className={"main-layout__body"}>
-            <div className={"main-layout__content-container"} ref={contentRef}>
+            <div className={"main-layout__content-container"}>
                 <div className={"main-layout__content"}>{children}</div>
             </div>
         </div>
